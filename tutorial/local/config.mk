96a7f30ff0187cd037c7e421fb98e9467b31e3d1
schtepf <schtepf@4e9de6a8-e6b3-4a20-ba07-35caea2ed9df>
2010-06-01 01:35:21 +0000

    basic directory structure; first check-in of tutorial slides
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/wordspace@1 4e9de6a8-e6b3-4a20-ba07-35caea2ed9df

---
+++ tutorial/local/config.mk
@@ -0,0 +1,10 @@
+##
+##  local configuration of Makefile for LaTeX-Beamer slides
+##
+
+## specify names of presentations here (with extension ".pdf")
+FILES := naacl2010_part1.pdf naacl2010_part2.pdf
+
+## directory where generated PDF documents can be installed
+INSTALL_DIR := ../pdf
+
