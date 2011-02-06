64f1ad9077bbeedf4e51f077e286bd465f783960
schtepf <schtepf@4e9de6a8-e6b3-4a20-ba07-35caea2ed9df>
2011-02-06 22:15:47 +0000

    sync minor, but urgently needed changes in dsm.projection()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/wordspace@28 4e9de6a8-e6b3-4a20-ba07-35caea2ed9df

--- pkg/wordspace/R/dsm_projection.R
+++ pkg/wordspace/R/dsm_projection.R
@@ -1,4 +1,4 @@
-dsm.projection <- function (model, method=c("svd", "rsvd", "asvd", "ri", "ri+svd"), n=NA, oversampling=NA, q=2, verbose=FALSE) {
+dsm.projection <- function (model, method=c("svd", "rsvd", "asvd", "ri", "ri+svd"), n=NA, oversampling=NA, q=2, verbose=FALSE, with.basis=FALSE) {
   method <- match.arg(method)
   if (is.matrix(model) || inherits(model, "Matrix")) {
     M <- model
@@ -25,10 +25,12 @@ dsm.projection <- function (model, method=c("svd", "rsvd", "asvd", "ri", "ri+svd
 
     if (verbose) cat(sprintf("SVD reduction to %d dimensions:\n", n))
     if (verbose) cat(" - SVD decomposition\n")
-    SVD <- svd(M, nu=n, nv=0) # we don't need right singular vectors for the dimensionality reduction
+    SVD <- svd(M, nu=n, nv=(if (with.basis) n else 0)) # we don't need right singular vectors for the dimensionality reduction
     if (verbose) cat(" - composing final matrix\n")
     S <- SVD$u %*% diag(SVD$d[1:n], n, n) # dimensionality-reduced matrix
+    if (with.basis) B <- SVD$v
     R2 <- SVD$d[1:n] * SVD$d[1:n] / sum(SVD$d * SVD$d) # proportion of variance "explained" by SVD dimensions
+    rm(SVD)
 
   } else if (method == "asvd") {
     ## -- approximated SVD based on random sample of rows
@@ -41,19 +43,21 @@ dsm.projection <- function (model, method=c("svd", "rsvd", "asvd", "ri", "ri+svd
     SVD <- svd(M.sub, nu=0, nv=n) # here we only need the right singular vectors
     if (verbose) cat(" - composing final matrix\n")
     S <- M %*% SVD$v  # V projects columns to first n latent dimensions
+    if (with.basis) B <- SVD$v
     rm(SVD)
     S <- as.matrix(S) # make sure result is an ordinary dense matrix (for efficient further processing)
-    R2 <- norm(S, "F")^2 / norm(M, "F")^2 # this should be the proportion of "explained" variance
+    R2 <- cumsum(colSums(S*S)) / norm(M, "F")^2 # this should be the proportion of "explained" variance
 
   } else if (method == "rsvd") {
 
     ## -- randomized SVD according to Halko, Martinsson & Tropp (2009, p. 9)
     ## We can apply the rSVD algorithm either to A = M or to A = t(M), depending on the format of M.
-    ## Preliminary testing suggests that the original algorithm (A = M) is suitable for a matrix with many columns,
+    ## Preliminary testing suggested that the original algorithm (A = M) is suitable for a matrix with many columns,
     ## while the transpose algorithm (A = t(M)) works better if the matrix has many rows and a limited number of columns.
-    ## For the time being, we use the original version if nR <= nC, and the transpose version otherwise.
+    ## With the current implementation, which uses SVD rather than QR decomposition to obtain an orthonormal basis,
+    ## there does not seem to be a substantial difference, so we currently always use the original algorithm.
     k2 <- min(oversampling * n, nR, nC)   # = 2*k in the paper
-    transpose <- !(nR <= nC)
+    transpose <- FALSE # previously: !(nR <= nC)
     if (verbose) cat(sprintf("Randomized SVD reduction%s to %d => %d dimensions:\n", if (transpose) " (transposed)" else "", k2, n))
     if (!transpose) {
       if (verbose) cat(" - sampling range of A\n") # -- original algorithm applied to A = M
@@ -64,8 +68,8 @@ dsm.projection <- function (model, method=c("svd", "rsvd", "asvd", "ri", "ri+svd
         if (verbose) cat(sprintf(" - power iteration #%d\n", i))
         Y <- M %*% crossprod(M, Y)
       }
-      if (verbose) cat(sprintf(" - QR decomposition of %d x %d matrix\n", nrow(Y), ncol(Y)))
-      Q <- qr.Q( qr(Y) )
+      if (verbose) cat(sprintf(" - orthonormal basis of %d x %d matrix\n", nrow(Y), ncol(Y)))
+      Q <- svd(Y, nu=k2, nv=0)$u  # orthonormal basis of rg(Y); SVD is faster than and as accurate as QR decomposition
       rm(Y)
       B <- crossprod(Q, M)
       if (verbose) cat(sprintf(" - SVD decomposition of %d x %d matrix\n", nrow(B), ncol(B)))
@@ -75,9 +79,10 @@ dsm.projection <- function (model, method=c("svd", "rsvd", "asvd", "ri", "ri+svd
       ## U <- Q %*% SVD$u # need to set nu=n above if we want to run this code (Uhat = SVD$u)
       ## --> now construct projected matrix from U and SVD$d
       S <- M %*% SVD$v
+      if (with.basis) B <- SVD$v
+      R2 <- cumsum(SVD$d^2) / norm(M, "F")^2
       rm(SVD)
       S <- as.matrix(S) # make sure result is an ordinary dense matrix
-      R2 <- norm(S, "F")^2 / norm(M, "F")^2
     } else {
       if (verbose) cat(" - sampling range of A\n") # -- transposed algorithm for A = t(M)
       Omega <- matrix(rnorm(k2*nR), k2, nR) # = t(Omega)
@@ -87,8 +92,8 @@ dsm.projection <- function (model, method=c("svd", "rsvd", "asvd", "ri", "ri+svd
         if (verbose) cat(sprintf(" - power iteration #%d\n", i))
         Y <- tcrossprod(Y, M) %*% M         # = t( (A * t(A))^i * A * Omega) ) = t(Y)
       }
-      if (verbose) cat(sprintf(" - QR decomposition of %d x %d matrix\n", ncol(Y), nrow(Y)))
-      Q <- qr.Q( qr(t(Y)) )                 # = orthonormal basis of rg(Y), from QR decomposition
+      if (verbose) cat(sprintf(" - orthonormal basis of %d x %d matrix\n", ncol(Y), nrow(Y)))
+      Q <- svd(Y, nu=0, nv=k2)$v  # orthonormal basis of rg(t(Y)); SVD is faster than and as accurate as QR decomposition
       rm(Y)
       B <- M %*% Q                          # = t( t(Q) * A ) = t(B)
       if (verbose) cat(sprintf(" - SVD decomposition of %d x %d matrix\n", nrow(B), ncol(B)))
@@ -96,9 +101,10 @@ dsm.projection <- function (model, method=c("svd", "rsvd", "asvd", "ri", "ri+svd
       rm(B)
       if (verbose) cat(" - composing final matrix\n")
       S <- M %*% Q %*% SVD$v                # t(t(U) * A) = t(A) * U = t(A) * Q * Uhat
+      if (with.basis) B <- Q %*% SVD$v
+      R2 <- cumsum(SVD$d^2) / norm(M, "F")^2
       rm(SVD)
       S <- as.matrix(S) # make sure result is an ordinary dense matrix
-      R2 <- norm(S, "F")^2 / norm(M, "F")^2
     }
   } else {
     stop("dimensionality reduction method '", method, "' has not been implemented yet")
@@ -106,6 +112,7 @@ dsm.projection <- function (model, method=c("svd", "rsvd", "asvd", "ri", "ri+svd
 
   rownames(S) <- rownames(M)
   colnames(S) <- paste(method, 1:n, sep="")
+  if (with.basis) attr(S, "basis") <- B
   attr(S, "R2") <- R2
   return(S)
  }
