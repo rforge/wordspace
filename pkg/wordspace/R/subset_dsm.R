398620ce1a74bf7d5520e8d3266423c97db28e89
schtepf <schtepf@4e9de6a8-e6b3-4a20-ba07-35caea2ed9df>
2014-02-23 22:47:48 +0000

    worspace v0.1-1: making progress with internal reorganisation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/wordspace@63 4e9de6a8-e6b3-4a20-ba07-35caea2ed9df

--- pkg/wordspace/R/subset_dsm.R
+++ pkg/wordspace/R/subset_dsm.R
@@ -1,24 +1,32 @@
-subset.dsm <- function (x, subset, select, drop.zeroes=FALSE, matrix.only=FALSE, ...) {
+subset.dsm <- function (x, subset=NULL, select=NULL, recursive=FALSE, drop.zeroes=FALSE, matrix.only=FALSE, envir=parent.frame(), ...) {
   info <- check.dsm(x, validate=TRUE) # make sure that rows/columns are consistent
+  if (recursive && matrix.only) stop("matrix.only=TRUE cannot be combined with recursive=TRUE")
 
-  # NB: subset and select conditions cannot be disabled by specifying NULL, since is.null()
-  #     would force evaluation of the expression in the current environment (!= eval below)
-  if (missing(subset)) {
+  while (recursive) {
+    force(envir) # fix parent environment to context of initial function call
+    y <- do.call(subset.dsm, list(x=x, subset=substitute(subset), select=substitute(select), recursive=FALSE, drop.zeroes=drop.zeroes, matrix.only=FALSE, envir=envir))
+    y.info <- check.dsm(y, validate=TRUE)
+    if (y.info$nrow == info$nrow && y.info$ncol == info$ncol) return(y)
+    x <- y
+    info <- y.info
+  }
+  
+  condition <- substitute(subset)
+  row.idx <- eval(condition, c(x$rows, x$globals), envir)
+  if (is.null(row.idx)) {
     row.idx <- 1:info$nrow
     update.nz.cols <- FALSE
   } else {
-    condition <- substitute(subset)
-    row.idx <- eval(condition, c(x$rows, x$globals), parent.frame())
     ## todo: check validity (either Boolean of correct length or numeric vector with indexes in range)
     update.nz.cols <- TRUE # rows may have been deleted, so nnzero counts for columns need to be updated
   }
   
-  if (missing(select)) {
+  condition <- substitute(select)
+  col.idx <- eval(condition, c(x$cols, x$globals), envir)
+  if (is.null(col.idx)) {
     col.idx <- 1:info$ncol
     update.nz.rows <- FALSE
   } else {
-    condition <- substitute(select)
-    col.idx <- eval(condition, c(x$cols, x$globals), parent.frame())
     ## todo: check validity (either Boolean of correct length or numeric vector with indexes in range)
     update.nz.rows <- TRUE # columns may have been deleted, so nnzero counts for rows need to be updated
   }
@@ -27,7 +35,7 @@ subset.dsm <- function (x, subset, select, drop.zeroes=FALSE, matrix.only=FALSE,
   if (is.logical(col.idx)) col.idx <- which(col.idx)
 
   if (drop.zeroes) {
-    M <- if (info$have.S) x$S else x$M  # primary data matrix (use scores if available, which may be sparser than frequencies)
+    M <- if (info$S$ok) x$S else x$M  # primary data matrix (use scores if available, which may be sparser than frequencies)
     is.nzero <- M[row.idx, col.idx, drop=FALSE] != 0
     nnzero.rows <- rowSums(is.nzero)
     nnzero.cols <- colSums(is.nzero)
@@ -42,25 +50,24 @@ subset.dsm <- function (x, subset, select, drop.zeroes=FALSE, matrix.only=FALSE,
   }
 
   if (matrix.only) {
-    M <- if (info$have.S) x$S else x$M  # matrix.only=TRUE: just return subset of the appropriate matrix
+    M <- if (info$S$ok) x$S else x$M  # matrix.only=TRUE: just return subset of the appropriate matrix
     return(M[row.idx, col.idx, drop=FALSE])
   }
 
-  # for small result sets, it's much more memory-efficient to construct a new DSM object from scratch
+  ## for small result sets, it is more memory-efficient to construct a new DSM object from scratch
   y <- list(rows=x$rows[row.idx, , drop=FALSE],   # mandatory components
             cols=x$cols[col.idx, , drop=FALSE],
-            globals=x$globals, locked=x$locked)
-  if ("N" %in% info$slots) y$N <- x$N             # optional components
-  if (info$have.M) y$M <- x$M[row.idx, col.idx, drop=FALSE]
-  if (info$have.S) y$S <- x$S[row.idx, col.idx, drop=FALSE]
+            globals=x$globals)
+  if (info$M$ok) y$M <- x$M[row.idx, col.idx, drop=FALSE]
+  if (info$S$ok) y$S <- x$S[row.idx, col.idx, drop=FALSE]
 
-  # if rows and/or columns may have been deleted, update the relevant nnzero counts
+  ## if rows and/or columns may have been deleted, update the relevant nonzero counts
   if (drop.zeroes) {
     y$rows$nnzero <- nnzero.rows  # we've already computed these above
     y$cols$nnzero <- nnzero.cols
   } else {
     if (update.nz.rows || update.nz.cols) {
-      is.nzero <- if (info$have.S) y$S != 0 else y$M != 0
+      is.nzero <- if (info$S$ok) y$S != 0 else y$M != 0
       if (update.nz.rows) y$rows$nnzero <- rowSums(is.nzero)
       if (update.nz.cols) y$cols$nnzero <- colSums(is.nzero)
     }
--- pkg/wordspace/R/t_dsm.R
+++ pkg/wordspace/R/t_dsm.R
@@ -1,9 +1,8 @@
 t.dsm <- function (x) {
-  .info <- check.dsm(x)
-  tx <- list(rows=x$cols, cols=x$rows, globals=x$globals, locked=x$locked)
-  if (!is.na(.info$N)) tx$N <- .info$N
-  if (.info$have.M) tx$M <- t(x$M)
-  if (.info$have.S) tx$S <- t(x$S)
+  info <- check.dsm(x)
+  tx <- list(rows=x$cols, cols=x$rows, globals=x$globals)
+  if (info$M$ok) tx$M <- t(x$M)
+  if (info$S$ok) tx$S <- t(x$S)
   class(tx) <- c("dsm", "list")
   return(tx)
 }
