f06d50d7316677aded6af470274b4a3419b54061
schtepf <schtepf@4e9de6a8-e6b3-4a20-ba07-35caea2ed9df>
2014-04-30 10:15:40 +0000

    Example data sets of verb-noun triples (BNC and DESC) replace BNC_Vobj DSM in wordspace package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/wordspace@85 4e9de6a8-e6b3-4a20-ba07-35caea2ed9df

--- datasets/examples_cooc.R
+++ datasets/examples_cooc.R
@@ -6,14 +6,14 @@
 library(wordspace)
 
 desc.vn <- read.delim("orig/desc_cc_vn_triples.txt.gz", header=FALSE, quote="", col.names=c("f", "noun", "rel", "verb"), colClasses=c("numeric", "character", "character", "character"), fileEncoding="utf8")
-DSM_VerbNounTriples_DESC <- desc.vn[, c("noun", "rel", "verb", "f")]
+DSM_VerbNounTriples_DESC <- subset(desc.vn, f >= 2, c("noun", "rel", "verb", "f"))
 
 str(DSM_VerbNounTriples_DESC)
 save(DSM_VerbNounTriples_DESC, file="data/DSM_VerbNounTriples_DESC.rda", compress="xz", compression_level=6)
 
 
 bnc.vn <- read.delim("orig/bnc_cc_vn_triples.txt.gz", header=FALSE, quote="", col.names=c("f", "noun", "rel", "verb", "mode"), colClasses=c("numeric", "character", "character", "character", "factor"), fileEncoding="utf8")
-DSM_VerbNounTriples_BNC <- bnc.vn[, c("noun", "rel", "verb", "f", "mode")]
+DSM_VerbNounTriples_BNC <- subset(bnc.vn, f >= 2, c("noun", "rel", "verb", "f", "mode"))
 
 str(DSM_VerbNounTriples_BNC)
 save(DSM_VerbNounTriples_BNC, file="data/DSM_VerbNounTriples_BNC.rda", compress="xz", compression_level=6)
