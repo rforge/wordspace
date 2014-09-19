02953a8a4d5bd54378d98143696430a13cb2f10c
schtepf <schtepf@4e9de6a8-e6b3-4a20-ba07-35caea2ed9df>
2014-09-19 02:10:47 +0000

    New version of DSM tutorial - part 3 (based on evaluation experiments by Gabriella Lapesa)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/wordspace@111 4e9de6a8-e6b3-4a20-ba07-35caea2ed9df

--- tutorial/local/config.mk
+++ tutorial/local/config.mk
@@ -3,7 +3,7 @@
 ##
 
 ## specify names of presentations here (with extension ".pdf")
-FILES := naacl2010_part1.pdf naacl2010_part2.pdf dsm_tutorial_part1.pdf
+FILES := naacl2010_part1.pdf naacl2010_part2.pdf dsm_tutorial_part1.pdf dsm_tutorial_part3.pdf
 
 ## directory where generated PDF documents can be installed
 INSTALL_DIR := ../pdf
