82f88e1c377b893a7e0b3efdbe8fd9ffca17619c
schtepf <schtepf@4e9de6a8-e6b3-4a20-ba07-35caea2ed9df>
2017-03-11 22:14:41 +0000

    Small updates to tutorial Part 1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/wordspace@176 4e9de6a8-e6b3-4a20-ba07-35caea2ed9df

--- tutorial/local/config.mk
+++ tutorial/local/config.mk
@@ -3,7 +3,7 @@
 ##
 
 ## specify names of presentations here (with extension ".pdf")
-FILES := naacl2010_part1.pdf naacl2010_part2.pdf dsm_tutorial_part1.pdf dsm_tutorial_part2.pdf dsm_tutorial_part3.pdf
+FILES := naacl2010_part1.pdf naacl2010_part2.pdf dsm_tutorial_part1.pdf dsm_tutorial_part2.pdf dsm_tutorial_part3.pdf dsm_tutorial_part4.pdf dsm_tutorial_part5.pdf
 
 ## directory where generated PDF documents can be installed
 INSTALL_DIR := ../pdf
